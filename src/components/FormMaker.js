d69735b018fbcf20324e4600c483fd961f301ed5
AbdulBsit <abasit325@gmail.com>
2020-03-16 18:40:33 +0530

    added segment functionality

--- src/components/FormMaker.js
+++ src/components/FormMaker.js
@@ -6,40 +6,153 @@ function FormMaker() {
     "product_desc",
     "product_price"
   ]);
+  const [segmentedField, setSegmentedField] = React.useState([]);
+  const [segments, setSegments] = React.useState([]);
   const [showPreview, setShowPreview] = React.useState(false);
   const [object, setObject] = React.useState({});
-  const generateJSON = e => {
+
+  const initSegment = () => {
+    setSegments([
+      ...segments,
+      {
+        title: document.getElementById("segmentTitle").value,
+        subtitle: document.getElementById("segmentSubTitle").value,
+        illustration: document.getElementById("segmentIllustration").value
+      }
+    ]);
+    document.getElementById("segmentTitle").value = "";
+    document.getElementById("segmentSubTitle").value = "";
+    document.getElementById("segmentIllustration").value = "";
+  };
+  const generateFormObject = e => {
+    setSegments(
+      segments.map((item, index) => {
+        var segmentIndex = document.getElementById(index).id;
+        var fieldIndex = textLayers.indexOf(
+          document.getElementById(index).value
+        );
+        setSegmentedField([
+          ...segmentedField,
+          document.getElementById(index).value
+        ]);
+        if (index == segmentIndex) {
+          if (item.fields !== undefined) {
+            return {
+              ...item,
+              fields: [
+                ...item.fields,
+                {
+                  name: document.getElementById(index).value,
+                  type: document.getElementById(fieldIndex + "type").value,
+                  label: document.getElementById(fieldIndex + "label").value,
+                  required: document.getElementById(fieldIndex + "required")
+                    .value,
+                  maxLength: document.getElementById(fieldIndex + "maxLenght")
+                    .value
+                }
+              ]
+            };
+          } else
+            return {
+              ...item,
+              fields: [
+                {
+                  name: document.getElementById(index).value,
+                  type: document.getElementById(fieldIndex + "type").value,
+                  label: document.getElementById(fieldIndex + "label").value,
+                  required: document.getElementById(fieldIndex + "required")
+                    .value,
+                  maxLength: document.getElementById(fieldIndex + "maxLenght")
+                    .value
+                }
+              ]
+            };
+        } else return item;
+      })
+    );
+  };
+
+  const previewForm = e => {
+    console.log("calling");
     e.preventDefault();
-    var obj = { fields: [] };
-    textLayers.map((item, index) => {
-      obj.fields.push({
-        name: item,
-        type: document.getElementById(index + "type").value,
-        label: document.getElementById(index + "label").value,
-        required: document.getElementById(index + "required").value,
-        maxLength: document.getElementById(index + "maxLenght").value
-      });
-    });
-    setObject(obj);
-    setShowPreview(true);
-    return false;
+    if (segments.length === 0) {
+      alert("Please add atleast 1 segment");
+      return false;
+    } else {
+      setShowPreview(true);
+      return false;
+    }
   };
   return (
-    <div
+    <form
+      onSubmit={previewForm}
       style={{
         display: "flex",
         justifyContent: "center",
         alignItems: "center"
       }}
     >
-      <form
+      {showPreview && (
+        <form
+          style={{
+            padding: 25,
+            backgroundColor: "pink",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center"
+          }}
+        >
+          {segments.map((item, index) => {
+            return (
+              <div style={{ backgroundImage: item.illustration }}>
+                <h2>{item.title}</h2>
+                <p>{item.subtitle}</p>
+                {item.fields !== undefined &&
+                  item?.fields.map((item, index) => {
+                    return (
+                      <div>
+                        <p>{item.label}</p>
+                        {item.required ? (
+                          <input
+                            type={
+                              item.type === "custom_no_input"
+                                ? "number"
+                                : "text"
+                            }
+                            id={item.name}
+                            maxLength={item.maxLength}
+                            required={item.required}
+                          />
+                        ) : (
+                          <input
+                            type={
+                              item.type === "custom_no_input"
+                                ? "number"
+                                : "text"
+                            }
+                            id={item.name}
+                            maxLength={item.maxLength}
+                          />
+                        )}
+                      </div>
+                    );
+                  })}
+              </div>
+            );
+          })}
+
+          <input type="submit" value="submit" />
+        </form>
+      )}
+      <div
         style={{
           display: "flex",
+          padding: 25,
           flexDirection: "column",
           justifyContent: "center",
           alignItems: "center"
         }}
-        onSubmit={generateJSON}
       >
         {textLayers.map((item, index) => {
           return (
@@ -71,35 +184,74 @@ function FormMaker() {
             </div>
           );
         })}
-        <input type="submit" value="Preview Form" />
-      </form>
-      {showPreview && (
-        <div
-          style={{
-            padding: 25,
-            backgroundColor: "pink",
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-            alignItems: "center"
-          }}
-        >
-          {object.fields.map((item, index) => {
-            return (
-              <div>
-                <h4>{item.label}</h4>
-                <input
-                  type={item.type === "custom_no_input" ? "number" : "text"}
-                  id={item.name}
-                  maxLength={item.maxLength}
-                  required={item.required}
-                />
-              </div>
-            );
-          })}
-        </div>
-      )}
-    </div>
+      </div>
+      <div
+        style={{
+          padding: 25,
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center"
+        }}
+      >
+        {segments.length !== 0 && (
+          <div>
+            {segments.map((item, index) => {
+              return (
+                <div>
+                  <p>Segment {index + 1}</p>
+                  <p>Title : {item.title}</p>
+                  <p>Subtitle : {item.subtitle}</p>
+                  <p>Illustration : {item.illustration}</p>
+                  {item.fields !== undefined &&
+                    item?.fields.map(item => <p>{item.name}</p>)}
+                  {textLayers.length === segmentedField.length ? null : (
+                    <div>
+                      <select id={index}>
+                        {textLayers.map((item, index) => {
+                          if (segmentedField.includes(item)) {
+                            return;
+                          }
+                          return (
+                            <option id={index} value={item}>
+                              {item}
+                            </option>
+                          );
+                        })}
+                      </select>
+                      <button onClick={generateFormObject}>Add field</button>
+                    </div>
+                  )}
+                </div>
+              );
+            })}
+          </div>
+        )}
+        {segmentedField.length === textLayers.length ? null : (
+          <div style={{ display: "flex", flexDirection: "column" }}>
+            <h2>Create Segments</h2>
+            <input
+              type="text"
+              id="segmentTitle"
+              placeholder="Enter Segment title"
+            />
+            <input
+              type="text"
+              id="segmentSubTitle"
+              placeholder="Enter Segment Sub title"
+            />
+            <input
+              type="url"
+              id="segmentIllustration"
+              placeholder="URL for illustration"
+            />
+
+            <button onClick={initSegment}>create segment</button>
+          </div>
+        )}
+      </div>
+      <input type="submit" value="Preview Form" />
+    </form>
   );
 }
 export default FormMaker;
